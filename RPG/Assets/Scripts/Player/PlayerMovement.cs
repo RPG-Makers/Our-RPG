2cc0dadbf3769fbbad280a3ef34a94f772b4bb2d
Alfie213 <arturbogoratov@gmail.com>
2022-11-13 22:12:38 +0300

    Changes
    Edited explorer and PlayerMovement.cs.

---
+++ RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+    }
+}
