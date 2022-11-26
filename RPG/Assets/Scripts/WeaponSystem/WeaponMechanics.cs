7b3afe72c1d53c441e944d2d291aa0b10205ab5f
Alfie213 <arturbogoratov@gmail.com>
2022-11-26 17:05:19 +0300

    WeaponMechanics
    Added weapon to player and WeaponMechanics.

--- RPG/Assets/Scripts/WeaponSystem/WeaponMechanics.cs
+++ RPG/Assets/Scripts/WeaponSystem/WeaponMechanics.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class WeaponMechanics : MonoBehaviour
 {
     [SerializeField] private WeaponStats weapon;
-    [SerializeField] private Transform weaponTransform;
+    [SerializeField] private GameObject weaponGo;
     [SerializeField] private LayerMask enemyLayer;
+    private Animator weaponAnimator;
+    private Transform weaponTransform;
+
+    private void Awake()
+    {
+        weaponAnimator = weaponGo.GetComponent<Animator>();
+        weaponTransform = weaponGo.transform;
+    }
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            weaponAnimator.Play("smth");
             Physics2D.OverlapCircle(weaponTransform.position, 1f, enemyLayer)?.gameObject.GetComponent<Health>().getHit(weapon.damage); // I'm really don't understanding, why it doesn't work if we are using same Vector2.
         }
     }
