a61bad0ffe8711533c6444fd5ce329e9f4bf67d4
Alfie213 <arturbogoratov@gmail.com>
2022-12-05 23:32:17 +0300

    Fix

--- RPG/Assets/Scripts/WeaponSystem/WeaponMechanics.cs
+++ RPG/Assets/Scripts/WeaponSystem/WeaponMechanics.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class WeaponMechanics : MonoBehaviour
 {
+    [SerializeField] private WeaponStats weapon;
+    [SerializeField] private GameObject weaponGo;
     [SerializeField] private LayerMask enemyLayer;
-    private Weapon weapon;
+    private Animator weaponAnimator;
+    private Transform weaponTransform;
 
     private void Awake()
     {
-        weapon = transform.GetChild(1).gameObject.GetComponent<Weapon>(); // Attention!
+        weaponAnimator = weaponGo.GetComponent<Animator>();
+        weaponTransform = weaponGo.transform;
     }
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            weapon.Attack(enemyLayer);
+            weaponAnimator.Play("smth");
+            Physics2D.OverlapCircle(weaponTransform.position, 1f, enemyLayer)?.gameObject.GetComponent<Health>().getHit(weapon.damage); // I'm really don't understanding, why it doesn't work if we are using same Vector2.
         }
     }
 }
--- RPG/Assets/Scripts/WeaponSystem/WeaponSword.cs
+++ RPG/Assets/Scripts/WeaponSystem/WeaponSword.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSword : Weapon
+public class WeaponSword : MonoBehaviour
 {
-    public override void Attack(LayerMask enemyLayer)
+    [SerializeField] private WeaponStats weapon;
+    private void Attack()
     {
-        Debug.Log("Sword Attack");
-        weaponAnimation.Play("smth");
-        // this string should be in the weapon.Attack(), because there is different logic for sword and bow (UPD: Already ok. Just saving for... idk).
-        Physics2D.OverlapCircle(transform.position, 1f, enemyLayer)?.gameObject.GetComponent<Health>().getHit(WeaponData.Damage); // I'm really don't understanding, why it doesn't work if we are using same Vector2.
+
     }
 }
