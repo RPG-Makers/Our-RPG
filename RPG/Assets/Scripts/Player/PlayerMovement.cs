42b20f996c78ba0e98fcd99d932edf1bcf0b1bb8
vadeeme <vadeeme@gmail.com>
2022-12-10 13:04:27 +0400

    Added Main Scene

--- RPG/Assets/Scripts/Player/PlayerMovement.cs
+++ RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-
     public static PlayerMovement Instance;
     [SerializeField] private float movementSpeed;
     private Rigidbody2D rb;
-    private Vector2 moveDirection;
+    public Animator animator;
+    Vector2 movement;
+    public SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
@@ -17,19 +18,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        movement = movement.normalized;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x); //run walk animation
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", Mathf.Abs(movement.sqrMagnitude));
+        
+        if(Input.GetKeyDown(KeyCode.A)) // flip the sprite. When we'll have left right mirror animatin, should turn this feature off and adjust the animator
+        {
+            
+            spriteRenderer.flipX = true;
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 
     public void SpeedUp(int value) //баф скорости от зелья в инвентаре
     {
         movementSpeed += value;
-        
     }
 }
