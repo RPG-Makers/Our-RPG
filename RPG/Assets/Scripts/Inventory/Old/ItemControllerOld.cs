f1a60e053f7d97fd83d5915dfd9626d7137e7f89
Alfie213 <arturbogoratov@gmail.com>
2022-12-16 01:26:48 +0300

    Unfinished Inventory system
    Need a revision.

--- RPG/Assets/Scripts/Inventory/Old/ItemControllerOld.cs
+++ RPG/Assets/Scripts/Inventory/Old/ItemControllerOld.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemController : MonoBehaviour
+public class ItemControllerOld : MonoBehaviour
 {
     public Item Item;
 }
