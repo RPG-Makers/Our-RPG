9d527d4c9b8e367c227d19386fc7560451508aea
Alfie213 <arturbogoratov@gmail.com>
2023-02-23 19:43:50 +0300

    Remake Inventory System (not finished)
    Better Drag&Drop work between UI Cells.

--- RPG/Assets/Scripts/Inventory/MyOwnInventory/CellUIMovler.cs
+++ RPG/Assets/Scripts/Inventory/MyOwnInventory/CellUIMovler.cs
@@ -31,39 +31,55 @@ public class CellUIMovler : MonoBehaviour
 
     public void Drop()
     {
-        //Debug.Log(this.transform.position);
         // If collide with underlayer - set on the cell;
         // Else Instantiate(_confirmation, this.transform.parent.parent);
 
-        List<Collider2D> colliders = new List<Collider2D>();
-        if (_collider.OverlapCollider(new ContactFilter2D().NoFilter(), colliders) > 0)
+        // There are two ways to solve this task.
+        // 1st (simple) is use Physics2D.OverlapPoint(Input.mousePosition).
+        // 2nd (a little bit difficult, but also simple, but not in 1 string) is use OverlapCollider(...).
+
+        #region 1st
+        //List<Collider2D> colliders = new List<Collider2D>();
+        //if (_collider.OverlapCollider(new ContactFilter2D().NoFilter(), colliders) > 0)
+        //{
+        //    Debug.Log($"Overlaped {colliders.Count} colliders.");
+        //    this.transform.position = FindClosestCollider(colliders).gameObject.transform.position;
+        //}
+        ///// <summary>
+        ///// Finds the closest Collider2D by postition.
+        ///// </summary>
+        ///// <param name="colliders">Input colliders</param>
+        ///// <returns>Closest collider</returns>
+        //private Collider2D FindClosestCollider(List<Collider2D> colliders)
+        //{
+        //    // Для меня лучший коллайдер тот, на котором стоит курсор. В итоге задача состоит в том, чтобы вернуть коллайдер, на котором находится курсор.
+        //    Collider2D bestCollider = new Collider2D();
+        //    foreach (Collider2D collider in colliders)
+        //    {
+        //        if (collider.OverlapPoint(Input.mousePosition))
+        //        {
+        //            bestCollider = collider;
+        //            // break???
+        //        }
+        //    }
+        //    return bestCollider;
+        //}
+        //else Debug.Log("Nothing overlaped.");
+        #endregion
+
+        Collider2D col = Physics2D.OverlapPoint(Input.mousePosition);
+        if (col == null)
         {
-            Debug.Log($"Задели {colliders.Count} коллайдеров");
-            this.transform.position = FindClosestCollider(colliders).gameObject.transform.position;
+            // Ну тогда вернём на прежнее место.
+            Debug.Log("null");
         }
         else
         {
-            Debug.Log("Ничё не задели");
+            Debug.Log("not null");
+            this.transform.position = col.gameObject.transform.position;
+            //Debug.Log($"New position is {col.gameObject.transform.position}.");
         }
     }
 
-    /// <summary>
-    /// Finds the closest Collider2D by postition.
-    /// </summary>
-    /// <param name="colliders"></param>
-    /// <returns></returns>
-    private Collider2D FindClosestCollider(List<Collider2D> colliders)
-    {
-        // Для меня лучший коллайдер тот, на котором стоит курсор. В итоге задача состоит в том, чтобы вернуть коллайдер, на котором находится курсор.
-        Collider2D bestCollider = new Collider2D();
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.OverlapPoint(Input.mousePosition))
-            {
-                bestCollider = collider;
-                Debug.Log("Нашли");
-            }
-        }
-        return bestCollider;
-    }
+
 }
