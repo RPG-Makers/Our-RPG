bd7f3acfbb596762e425a5f18042044823287e3f
Alfie213 <arturbogoratov@gmail.com>
2023-02-23 17:32:19 +0300

    Remake Inventory System (not finished)
    Start of Drag&Drop System.

--- RPG/Assets/Scripts/Inventory/MyOwnInventory/CellUIMovler.cs
+++ RPG/Assets/Scripts/Inventory/MyOwnInventory/CellUIMovler.cs
@@ -1,18 +1,29 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Class, that represents Drag&Drop system for inventory cells.
 /// </summary>
 public class CellUIMovler : MonoBehaviour
 {
+    [SerializeField] private Button _confirmation;
+    [SerializeField] private GameObject _container;
+    Collider2D _someCol;
+
     public void Click()
     {
         Debug.Log($"На меня нажали, во мне {GetComponentInChildren<TextMeshProUGUI>().text} предметов");
     }
 
-    public void Move()
+    public void Drag()
     {
         transform.position = Input.mousePosition;
+    }
+
+    public void Drop()
+    {
+        Debug.Log(this.transform.position);
+        Instantiate(_confirmation, this.transform.parent.parent);
     }
 }
