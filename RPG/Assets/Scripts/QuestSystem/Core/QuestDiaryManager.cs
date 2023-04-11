740668da838553238446af8e795bf69865df20cd
Alfie213 <arturbogoratov@gmail.com>
2023-04-11 15:51:27 +0300

    QuestDiary
    Instantiating buttons of quests.

--- RPG/Assets/Scripts/QuestSystem/Core/QuestDiaryManager.cs
+++ RPG/Assets/Scripts/QuestSystem/Core/QuestDiaryManager.cs
@@ -12,20 +12,29 @@ public class QuestDiaryManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _questName;
     [SerializeField] private TextMeshProUGUI _questDescription;
 
-    public void InitializeDetails(string questName, string questDescription)
-    {
-        _questName.text = questName;
-        _questDescription.text = questDescription;
-    }
-
-    public void InitializeQuests(IEnumerable<Quest> quests)
-    {
-
-    }
+    [Header("QuestButtons")]
+    [SerializeField] private Transform _questButtonsParent;
+    [SerializeField] private GameObject _questButtonPrefab;
 
     public void EnableQuestDiaryUI()
     {
         _content.SetActive(true);
+        InitializeQuestButtons(GameManager.Instance.PlayerQuest.Data.Quests); // здесь нужно запросить текущие квесты и отобразить.
+    }
+
+    private void InitializeQuestButtons(IEnumerable<Quest> quests)
+    {
+        foreach (var quest in quests)
+        {
+            GameObject questButton = Instantiate(_questButtonPrefab, _questButtonsParent);
+            questButton.GetComponentInChildren<TextMeshProUGUI>().text = quest.QuestData.Name;
+        }
+    }
+
+    private void InitializeDetails(string questName, string questDescription) // А эту штучку будем привязывать к кнопкам квестов.
+    {
+        _questName.text = questName;
+        _questDescription.text = questDescription;
     }
 
     public void Disable()
