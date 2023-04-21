6ac99a7bacd6ea31015ffa1173550e34ba707314
Alfie213 <arturbogoratov@gmail.com>
2023-04-21 09:20:11 +0300

    Changed Player's components
    -PlayerQuestUI;
    +PlayerQuest: added PlayerQuestData.

--- RPG/Assets/Scripts/QuestSystem/Core/Player/PlayerQuest.cs
+++ RPG/Assets/Scripts/QuestSystem/Core/Player/PlayerQuest.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerQuestUI))]
 public class PlayerQuest : MonoBehaviour
 {
     [SerializeField] private PlayerQuestData _data;
