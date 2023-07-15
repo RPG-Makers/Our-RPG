c9527b982a7ebad4d01433f56850fc5126969b33
Alfie213 <arturbogoratov@gmail.com>
2023-07-15 13:11:57 +0300

    Refactoring inventory
    Rename variable.

--- RPG/Assets/Scripts/Inventory/MyOwnInventory/Data/Inventory/InventoryData.cs
+++ RPG/Assets/Scripts/Inventory/MyOwnInventory/Data/Inventory/InventoryData.cs
@@ -1,17 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "InventoryData", menuName = "Inventory/InventoryData")]
 public class InventoryData : ScriptableObject
 {
-    public CellInventory[] cells;
-    private void Awake()
-    {
-        foreach (CellInventory cell in cells)
-        {
-
-        }
-    }
+    public CellInventory[] Cells;
 }
